38da25acf63fed41d9ca8daf40c9170e088e8f05
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-11-06 16:16:17 -0500

    save

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,12 +15,12 @@ export class HomePage {
 
   ionViewDidLoad(){
     let mapEle = document.getElementById('map');
-    let map = new google.maps.Map(mapEle, {
+    this.map = new google.maps.Map(mapEle, {
       center: {lat: 4.5981, lng: -74.0758},
       zoom: 12
     });
 
-    google.maps.event.addListenerOnce(map, 'idle', () => {
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
       mapEle.classList.add('show-map');
       google.maps.event.trigger(mapEle, 'resize');
     });
