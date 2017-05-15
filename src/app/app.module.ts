8b21aa836bebe804ee5a7d3ff4ef14544f52a55d
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-05-14 20:12:30 -0500

    upgrade to ionic 3.2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
-  providers: []
+  providers: [
+    SplashScreen,
+    StatusBar,
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage,NavController } from 'ionic-angular';
 
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+declare var google;
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,19 +15,43 @@ export class HomePage {
   map: any;
 
   constructor(
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    public geolocation: Geolocation
   ) {}
 
   ionViewDidLoad(){
-    let mapEle = document.getElementById('map');
+    this.obtenerPosicion();
+  }
+
+  obtenerPosicion():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  loadMap(postion: Geoposition){
+    let latitude = postion.coords.latitude;
+    let longitude = postion.coords.longitude;
+    
+    let mapEle: HTMLElement = document.getElementById('map');
+
+    let myLatLng = {lat: latitude, lng: longitude};
+
     this.map = new google.maps.Map(mapEle, {
-      center: {lat: 4.5981, lng: -74.0758},
+      center: myLatLng,
       zoom: 12
     });
 
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Hello World!'
+      });
       mapEle.classList.add('show-map');
-      google.maps.event.trigger(mapEle, 'resize');
     });
   }
 }
